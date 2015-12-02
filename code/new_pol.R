90f5e4a2cecd761f31f55fa5f699a74551288c84
tom13878 <tomas.morley@wur.nl>
2015-12-02 09:19:17 +0100

    Added some constituency name changes to ward constituency link file

--- code/new_pol.R
+++ code/new_pol.R
@@ -32,7 +32,7 @@ table(map_wards %in% toupper(df$ward))
 missing <- map_data[!(map_wards %in% toupper(df$ward)),]
 
 # -------------------------------------
-# check which regions are responsible for th
+# check which regions are responsible for the
 # missing values
 
 table(missing$Region_Nam[drop=TRUE])
@@ -90,3 +90,56 @@ missing[missing$Region_Nam %in% "Tabora",]
 
 # missing twenty two wards
 missing[missing$Region_Nam %in% "Arusha",]
+
+# -------------------------------------
+# compare with NEC constituency, ward 
+# maps to complete the constituency ward 
+# table
+plot(TZA[TZA@data$Region_Nam %in% "Arusha",])
+plot(TZA[TZA@data$Region_Nam %in% "Dodoma",])
+plot(TZA[TZA@data$Region_Nam %in% "Kilimanjaro",])
+plot(TZA[TZA@data$Region_Nam %in% "Tanga",])
+plot(TZA[TZA@data$Region_Nam %in% "Morogoro",])
+plot(TZA[TZA@data$Region_Nam %in% "Lindi",])
+plot(TZA[TZA@data$Region_Nam %in% "Ruvuma",])
+plot(TZA[TZA@data$Region_Nam %in% "Kagera",])
+plot(TZA[TZA@data$Region_Nam %in% "Kigoma",])
+plot(TZA[TZA@data$Region_Nam %in% "Manyara",])
+plot(TZA[TZA@data$Region_Nam %in% "Mara",])
+plot(TZA[TZA@data$Region_Nam %in% "Mbeya",])
+plot(TZA[TZA@data$Region_Nam %in% "Mtwara",])
+plot(TZA[TZA@data$Region_Nam %in% "Mwanza",])
+plot(TZA[TZA@data$Region_Nam %in% "Njombe",])
+plot(TZA[TZA@data$Region_Nam %in% "Rukwa",])
+plot(TZA[TZA@data$Region_Nam %in% "Shinyanga",])
+plot(TZA[TZA@data$Region_Nam %in% "Singida",])
+plot(TZA[TZA@data$Region_Nam %in% "Tabora",])
+
+# -------------------------------------
+# plots for finding missing wards
+
+sengerema <- TZA[TZA@data$District_N %in% "Sengerema",]
+Ibisabageni <- TZA[TZA@data$Ward_Nam %in% "Ibisabageni",]
+
+col <- toupper(sengerema@data$Ward_Name) %in% toupper(c("Ibisabageni"))
+plot(sengerema, col=col)
+
+# -------------------------------------
+# matching constituency names
+# read in the constituency data for 2010
+
+# clearl a problem with the constituency data, wards where constituencies should be
+
+leg10 <- read.table("C:/Users/Tomas/Documents/LEI/pol/data/leg10.txt", header=TRUE)
+tst <- unique(select(leg10, region, const))
+
+# cut out the islands
+islands <- c("Kaskazini Unguja", "Kusini Unguja", "Mjini Magharibi", "Kaskazini Pemba", "Kusini Pemba")
+islands <- toupper(islands)
+tst <- tst[!tst$region %in% islands, ]
+
+# check what is still missing
+missing <- !toupper(tst$const) %in% toupper(df$con)
+sum(missing) # 35
+tst[missing, ]
+
