dc8859cb4175b36f21060e8af0a64ec7f7b044c7
tom13878 <tomas.morley@wur.nl>
2016-07-18 15:54:14 +0200

    Ammended file to fully match up with the LSMS data regions

--- code/pol20104analysis.R
+++ code/pol20104analysis.R
@@ -12,11 +12,36 @@ prez2010 <- readRDS(file.path(dataPath, "data/prez2010/prez2010.rds"))
 # In many places there are Swahili names
 # we remove this part of the name
 
-prez2010$dis <- gsub("JIJI LA ", "", prez2010$dis)
 prez2010$dis <- gsub("WILAYA YA ", "", prez2010$dis)
-prez2010$dis <- gsub("MANISPAA YA ", "", prez2010$dis)
+dar <- prez2010$reg %in% "DAR-ES-SALAAM"
+prez2010$dis[dar] <- gsub("MANISPAA YA ", "", prez2010$dis[dar])
 
+urban <- c(grep("MANISPAA YA ", prez2010$dis), grep("JIJI LA ", prez2010$dis), grep("MJI WA ", prez2010$dis))
+prez2010$dis[urban] <- gsub("MANISPAA YA ", "", prez2010$dis[urban])
+prez2010$dis[urban] <- gsub("JIJI LA ", "", prez2010$dis[urban])
+prez2010$dis[urban] <- gsub("MJI WA ", "", prez2010$dis[urban])
+prez2010$dis[urban] <- paste(prez2010$dis[urban], "URBAN", sep=" ")
 
+# --------------------------------------
+# in the LSMS data districts there are some slight
+# changes compared to the electoral data districts
+# -------------------------------------
+
+# Siha is still part of Hai in the LSMS data
+prez2010$dis <- gsub("SIHA", "HAI", prez2010$dis)
+
+# the district called dodoma rural is
+# actualy comprised of two districts 
+# called BAHI and CHAMWINO
+prez2010$dis <- gsub("BAHI", "DODOMA RURAL", prez2010$dis)
+prez2010$dis <- gsub("CHAMWINO", "DODOMA RURAL", prez2010$dis)
+
+# in the LSMS data the district called 
+# LONGIDO is actually a part of MONDULI
+
+prez2010$dis <- gsub("LONGIDO", "MONDULI", prez2010$dis)
+
+# -------------------------------------
 # summarise the data by district
 prez2010_dis <- group_by(prez2010, reg, dis, party) %>%
   summarise(votes=sum(votes)) 
@@ -48,7 +73,7 @@ prez2010_dis <- group_by(prez2010_dis, reg, dis) %>%
 # next largest party
 prez2010 <- left_join(prez2010_dis, prez2010_dis_All)
 
-rm(prez2010_dis, prez2010_dis_All)
+rm(prez2010_dis, prez2010_dis_All, urban, dataPath, dar)
 
 # save the results for use in analysis later
 # saveRDS(prez2010_dis_All, file.path(dataPath, "data/prez2010/prez2010_dis_All.rds"))
